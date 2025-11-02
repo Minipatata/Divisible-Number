0d52d51e73ee3195ed030652c31de6bb767d4f62
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-11-02 12:21:10 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#Write to check a number is divisible by another number.
+num_1=int(input("Enter a number"))
+num_2=int(input("Enter another number"))
+if num_1%num_2==0:
+     print(num_1,"is divisible by",num_2)
+else:
+     print("It is not divisible")
